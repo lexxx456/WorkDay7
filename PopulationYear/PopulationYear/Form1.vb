f6c2011eaa63c921d1ba756e28c0cf6c36b9fda2
idsc3101 <idsc3101@gmail.com>
2014-10-08 17:11:35 -0500

    workday participation

--- PopulationYear/PopulationYear/Form1.vb
+++ PopulationYear/PopulationYear/Form1.vb
@@ -3,7 +3,13 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As Object, e As EventArgs) Handles btnPopulation.Click
-        MessageBox.Show("The answer: not implemented yet!")
+        Dim pop As Double = 7000
+        Dim year As Integer = 2014
+        Do While (pop > 6)
+            pop /= 2
+            year -= 50
+        Loop
+        MessageBox.Show(CStr(year))
 
     End Sub
 End Class
